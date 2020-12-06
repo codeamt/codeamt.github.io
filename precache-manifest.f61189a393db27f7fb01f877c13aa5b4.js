a1feeeb7812621efce3c51d1f9f0d7dca686c67b
AnnMargaret Tutu <amt@blaqbox.us>
2020-12-05 22:25:25 -0500

    committing changes

---
+++ precache-manifest.f61189a393db27f7fb01f877c13aa5b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49640667757f83f7bf405fbcd67a6f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8233c6b4cbab057cc06",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3ec59f796202a30a295a",
+    "url": "/static/js/2.be1fb1f1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.be1fb1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8233c6b4cbab057cc06",
+    "url": "/static/js/main.d887ebd7.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "d7849df06d7cbce46cafb4ca0ee0067c",
+    "url": "/static/media/Tutu.AMT.Fullstack_Resume.d7849df0.pdf"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
